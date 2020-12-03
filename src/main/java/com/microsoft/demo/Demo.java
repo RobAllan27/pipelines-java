0b5324e81e76b4b16f88db548d76cc7579d9f676
Rob Allan <Rob Allan>
2020-12-03 17:44:44 +1100

    First changes to add in new test cases

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,12 +1,20 @@
 package com.microsoft.demo;
 
 public class Demo {
-    public void DoSomething(boolean flag){
+    public void doSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
             return;
         }
-
         System.out.println("I am not covered");
+    }
+    
+    public boolean doSomethingElse(boolean flag){
+        if(flag){
+            System.out.println("I am covered in the something else");
+            return true;
+        }
+        System.out.println("I am not covered ine the something else");
+        return false;
     }
 }
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,14 +1,27 @@
 import com.microsoft.demo.Demo;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class MyTest {
     @Test
     public void test_method_1() {
         Demo d = new Demo();
-        d.DoSomething(true);
+        d.doSomething(true);
     }
 
     @Test
     public void test_method_2() {
+    }
+    
+    @Test
+    public void test_method_3() {
+        Demo d = new Demo();
+        Assert.assertTrue(d.doSomethingElse(true));
+    }
+
+    @Test
+    public void test_method_4() {
+        Demo d = new Demo();
+        Assert.assertFalse(d.doSomethingElse(false));
     }
 }
